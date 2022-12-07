68643ea775812562accab0a197895e7de36fe9db
Alexandra Pavel <alexandrapavel.ct@gmail.com>
2022-12-07 11:58:00 +0200

    upgrade to netcore 2.1

--- ChristmasJoy.App/Program.cs
+++ ChristmasJoy.App/Program.cs
@@ -9,22 +9,21 @@ namespace ChristmasJoy.App
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+          CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureAppConfiguration((builderContext, config) =>
-                {
-                  IHostingEnvironment env = builderContext.HostingEnvironment;
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((builderContext, config) =>
+            {
+              IHostingEnvironment env = builderContext.HostingEnvironment;
 
-                  config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                })
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+              config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            })
+            .UseIISIntegration()
+            .UseStartup<Startup>();
     }
 }
